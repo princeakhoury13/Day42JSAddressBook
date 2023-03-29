1ce581d0e86d33f02279e475f820175ced293b4a
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-29 13:56:45 +0530

    added: new address book with contacts

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,40 @@
-console.log("Welcome to Day 42 Assignment master branch")
+class Contact {
+  constructor(firstName, lastName, city, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.city = city;
+    this.phoneNumber = phoneNumber;
+  }
+}
+
+class AddressBook {
+  constructor() {
+    this.contacts = [];
+  }
+  
+  addContact(contact) {
+    this.contacts.push(contact);
+  }
+  
+  removeContact(index) {
+    this.contacts.splice(index, 1);
+  }
+  
+  getContact(index) {
+    return this.contacts[index];
+  }
+  
+  getAllContacts() {
+    return this.contacts;
+  }
+}
+
+// example usage
+const john = new Contact('Prince', 'Praveen', 'New York', '8369315555');
+const jane = new Contact('Tony', 'Stark', 'Los Angeles', '45454224945');
+const book = new AddressBook();
+
+book.addContact(john);
+book.addContact(jane);
+
+console.log(book.getAllContacts()); 
